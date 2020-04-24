4385287893279681e9c06c37066dbdd5139ff90b
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-04-24 16:10:29 -0400

    Removed the trapezoid. Added buttons for link to code and link to the app. Added technologies invovled for each project.

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 // Add logic to open links when project tile is clicked.
 
 // Open link in new tab given the id of the projectTile in string.
-function openLink(projectTile){
-    let elem = document.getElementById(projectTile);
-    let link = elem.getAttribute("href");
-    window.open(link, '_blank');
-}
+//function openLink(projectTile){
+//    let elem = document.getElementById(projectTile);
+//    let link = elem.getAttribute("href");
+//    window.open(link, '_blank');
+//}
 
