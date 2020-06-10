f85968b08f702ee02aa79507ffd9b7df629a46e8
Winnie Lam <winnielam@Winnies-MacBook-Air.local>
2020-06-10 17:08:17 -0400

    Changed padding on welcome-section when screen is small. Integrated with Vue.js.

--- script/main.js
+++ script/main.js
@@ -1,9 +1,62 @@
-// Add logic to open links when project tile is clicked.
+window.onload = function () {
+    const welcomeMessage = new Vue({
+        el: '#welcome-message',
+        data: {
+        message: 'I am software developer that believes in the KISS (Keep It Simple Stupid) principle.'
+        }
+    });
+    
+    const projectMyLibrary = new Vue({
+        el: '#project-mylibrary',
+        data : {
+            name: 'MyLibrary',
+            techs: ['jQuery', 'Bootstrap', 'JavaScript', 'CSS', 'HTML', 'Mocha', 'Chai', 'Firebase'],
+        }
+    });
 
-// Open link in new tab given the id of the projectTile in string.
-//function openLink(projectTile){
-//    let elem = document.getElementById(projectTile);
-//    let link = elem.getAttribute("href");
-//    window.open(link, '_blank');
-//}
+    const projectBlog = new Vue({
+        el: '#project-blog',
+        data : {
+            name: 'Software KISS (Blog)',
+            techs: ['JavaScript', 'CSS', 'HTML', 'Heroku', 'Express', 'PostgreSQL'],
+           
+        }
+    });
 
+    const projectSurveyForm = new Vue({
+        el: '#project-survey-form',
+        data : {
+            name: 'Survey Form',
+            techs: ['JavaScript', 'CSS', 'HTML'],
+           
+        }
+    });
+
+    const projectTechDocPage = new Vue({
+        el: '#project-technical-documentation-page',
+        data : {
+            name: 'Technical Documentation Page',
+            techs: ['Bootstrap', 'CSS', 'HTML'],
+           
+        }
+    });
+
+    const projectRandomQuoteMachine = new Vue({
+        el: '#project-random-quote-machine',
+        data : {
+            name: 'Random Quote Machine',
+            techs: ['React', 'Bootstrap', 'JavaScript', 'CSS', 'HTML'],
+           
+        }
+    });
+
+    const projectMarkdownPreviewer = new Vue({
+        el: '#project-markdown-previewer',
+        data : {
+            name: 'Markdown Previewer',
+            techs: ['React', 'Bootstrap', 'JavaScript', 'CSS', 'HTML'],
+           
+        }
+    });
+
+}
